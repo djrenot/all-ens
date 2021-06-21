92ca2dd4ead152671c286e98a71b98af27b8679a
djrenot <kiritori.ch@gmail.com>
2021-06-21 22:53:24 +0900

    Added latest update function

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 function latest_update() {
   let updated = document.lastModified;
   var footer_updated = document.getElementById('updated');
   return footer_updated.insertAdjacentHTML('afterbegin',updated);
 }
+// excute when load
+latest_update();
