40e721de9c7246a167c4e8c15af0fea9e9942a6e
djrenot <kiritori.ch@gmail.com>
2021-06-26 22:41:10 +0900

    updated top page content

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 function latest_update() {
   let updated = document.lastModified;
-  var footer_updated = document.getElementById('updated');
+  var footer_updated = document.getElementById('updated_date');
   return footer_updated.insertAdjacentHTML('afterbegin',updated);
 }
 // excute when load
