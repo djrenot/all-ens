bd401cac3bf2ff1962b5260110b994d55f199e9d
djrenot <kiritori.ch@gmail.com>
2021-07-04 23:54:34 +0900

    added function to get tweet (not working yet)

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,20 @@ function latest_update() {
 }
 // excute when load
 latest_update();
+
+
+// get user data
+function get_tweet(){
+  // removed headers
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.twitter.com/2/users/2848709654/tweets", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
