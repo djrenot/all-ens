3168be78bf7d9dec963f9bf9b793b23fffe02abe
djrenot <kiritori.ch@gmail.com>
2021-07-05 21:59:46 +0900

    installed nodejs but not working :)

--- README.md
+++ README.md
@@ -15,4 +15,8 @@
 ## references
 - [Headers](https://developer.mozilla.org/en-US/docs/Web/API/Headers)
   - [Content-Security-Policy](https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Content-Security-Policy)
+    - [Reason: CORS header 'Access-Control-Allow-Origin' missing](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin)
+  - [understanding cors](https://qiita.com/att55/items/2154a8aad8bf1409db2b)
 - [css error](https://qiita.com/baby-0105/items/0356b0af4ab4585d86c4)
+- [ReferenceError](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cant_access_lexical_declaration_before_init)
+- [import instead of require](https://learn.coderslang.com/0021-nodejs-require-is-not-defined-error/)
--- assets/app.js
+++ assets/app.js
@@ -8,17 +8,26 @@ latest_update();
 
 
 // get user data
+
 function get_tweet(){
-  // removed headers
+  // const fetch = require("node-fetch");
+  import fetch from 'node-fetch';
   
-  var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
+  const token =  "Bearer AAAAAAAAAAAAAAAAAAAAAJFdQgEAAAAAbcgEdZAvbDqNgbbCVZ%2BGHJcFJoQ%3Dc4Wis2U3krMu3iGlxtEnpMpIPq8fgzl5JLUErlqPEe5WCRu6E9"; // Replace BEARER_TOKEN with your token
+  const method = "GET";
+  const options = {
+    method: method,
+    headers: {
+      "Content-type": "application/json",
+      Authorization: token,
+    },
   };
   
-  fetch("https://api.twitter.com/2/users/2848709654/tweets", requestOptions)
-    .then(response => response.text())
+  fetch("https://api.twitter.com/2/users/2848709654/tweets" , options)
+    .then(response => {
+        console.log(response);
+    })
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 }
+get_tweet()
