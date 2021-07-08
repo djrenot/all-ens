52a48625f1850bd0d8d62d2da34aa849d2c0ad4e
djrenot <kiritori.ch@gmail.com>
2021-07-08 23:48:09 +0900

    make get_tweet works but not on the target page

--- assets/app.js
+++ assets/app.js
@@ -4,15 +4,13 @@ function latest_update() {
   return footer_updated.insertAdjacentHTML('afterbegin',updated);
 }
 // excute when load
-latest_update();
+// latest_update();
 
 
 // get user data
-
 function get_tweet(){
-  // const fetch = require("node-fetch");
-  import fetch from 'node-fetch';
-  
+  // import fetch from 'node-fetch';
+  const fetch = require("node-fetch");
   const token =  "Bearer AAAAAAAAAAAAAAAAAAAAAJFdQgEAAAAAbcgEdZAvbDqNgbbCVZ%2BGHJcFJoQ%3Dc4Wis2U3krMu3iGlxtEnpMpIPq8fgzl5JLUErlqPEe5WCRu6E9"; // Replace BEARER_TOKEN with your token
   const method = "GET";
   const options = {
@@ -23,11 +21,11 @@ function get_tweet(){
     },
   };
   
-  fetch("https://api.twitter.com/2/users/2848709654/tweets" , options)
-    .then(response => {
-        console.log(response);
-    })
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  const a = fetch("https://api.twitter.com/2/users/2848709654/tweets" , options)
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 get_tweet()
